0dc501c1f30e419a8bc5108e5e7fb90f90dca89f
Auro-rium <ishtrivedi18@gmail.com>
2026-08-08 23:21:43 +0530

    chore: remove frontend package from backend branch

--- canary/src/features/releases/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { CoverageSummary, type CoverageSummaryProps } from "./CoverageSummary";
-export { DecisionBadge, type DecisionBadgeProps } from "./DecisionBadge";
-export { RegressionCounts, type RegressionCountsProps } from "./RegressionCounts";
-export { SecurityScoreDelta, type SecurityScoreDeltaProps } from "./SecurityScoreDelta";
